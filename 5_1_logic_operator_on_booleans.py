15d6c822db38abb9927e34ad8df1f57e7e2a0518
amirsh7000 <amirsh700@gmail.com>
2018-04-28 13:09:45 +0430

    Update 5_1_logic_operator_on_booleans.py
    عملیات مشابه برای دو متغیر جدید انجام شد

--- 5_1_logic_operator_on_booleans.py
+++ 5_1_logic_operator_on_booleans.py
@@ -8,6 +8,18 @@ print("a & b is {0}".format(a and b)) # True if both of them are True
 print("a or b is {0}".format(a or b)) # True if one or both of them are True
 
 
+
+i= false
+j= true
+print("i is {0}".format(i))
+print("j is {0}".format(j))
+print("~i is {0}".format(not i))
+print("~j is {0}".format(not j))
+print("i & j is {0}".format(i and j))
+print("i or j is {0}".format(i or j))
+
+
+
 #################################################
 ## A     ### B     ### A and B ### A or B #######
 ## True  ### True  ### True    ### True   #######
