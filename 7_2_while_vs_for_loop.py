147516c3a334389f1fefa6731acf589f875bfe95
amirsh7000 <amirsh700@gmail.com>
2018-04-28 13:16:50 +0430

    Update 7_2_while_vs_for_loop.py
    عملیات مشابه برای ماه انجام شد

--- 7_2_while_vs_for_loop.py
+++ 7_2_while_vs_for_loop.py
@@ -9,3 +9,16 @@ print("inside for loop")
 year=int(input("please enter year\n"))
 for x in range(temp_year,2029,2):
     print(x)
+    
+    
+month=int(input("please enter month\n"))
+temp_month=month
+while month<12:
+    month+=1
+    print(month)
+
+print("inside for loop")
+
+month=int(input("please enter month\n"))
+for x in range(temp_month,12,2):
+    print(x)
