8bcb66a5a8ae7c33e2ba1e407952f6cb8859aff0
amirsh7000 <amirsh700@gmail.com>
2018-04-28 13:05:05 +0430

    Update 3_1_input.py
    خطوط 7 و 8 و 16 و 17 اضافه شد

--- 3_1_input.py
+++ 3_1_input.py
@@ -4,8 +4,15 @@
 text = input("enter what you want\n")
 print(5*text)
 
+string = input("enter a string\n")
+print(string)
+
 ##########################
 # get a number from user #
 ##########################
 number=int(input("enter an integer\n"))
 print(5*number)
+
+integer = int(input("inter an integer\n"))
+print(4+integer)
+
