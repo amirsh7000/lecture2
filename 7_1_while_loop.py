9c5ffbe8239f884a239f7ca6bf7e78a4ba1e552a
amirsh7000 <amirsh700@gmail.com>
2018-04-28 13:13:43 +0430

    Update 7_1_while_loop.py
    تنها چیزی که به ذهنم رسید تغییر شرط while بود

--- 7_1_while_loop.py
+++ 7_1_while_loop.py
@@ -6,3 +6,11 @@ while a != "Ali":
     print("oops you are not a legal user")
     a = input("please enter your name\n")
 print("thanks for your registration")
+
+
+a=input("please enter your name\n")
+while a != "amir":
+    print("oops you are not a legal user")
+    a = input("please enter your name\n")
+print("thanks for your registration")
+
