cd981c12e26d084f4f619e41a2ff7f71a72302e3
amirsh7000 <amirsh700@gmail.com>
2018-04-28 13:07:25 +0430

    Update 4_1_comparison_operator.py
    عملیات مشابه برای ذو متغیر جدید پیاده سازی شد

--- 4_1_comparison_operator.py
+++ 4_1_comparison_operator.py
@@ -9,3 +9,13 @@ print("i <= j --> {0}".format(i<=j))
 print("i >= j --> {0}".format(i>=j))
 print("i is equal to j --> {0}".format(i==j))
 print("i is not equal to j --> {0}".format(i!=j))
+
+
+k=8
+s=1
+print("k < s --> {0}".format(k<s))
+print("k > s --> {0}".format(k>s))
+print("k <= s --> {0}".format(k<=s))
+print("k >= s --> {0}".format(k>=s))
+print("k is equal to s --> {0}".format(k==s))
+print("k is not equal to s --> {0}".format(k!=s))
